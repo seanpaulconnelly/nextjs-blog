8b70e691d2cc95e8e608d9ede4d563a0dc77bb9f
Sean Connelly <sean@seanconnelly.me>
2023-11-01 14:14:17 -0400

    fixing 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Custom404() {
