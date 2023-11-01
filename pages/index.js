57c6b54701e52f3686b05298883f6f1b33dff0fe
Sean Connelly <sean@seanconnelly.me>
2023-11-01 14:13:23 -0400

    another case update for vercel deployment log

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout';
+import Layout, { siteTitle } from '../components/layout';
 import Link from 'next/link';
-import Date from '../components/Date';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
